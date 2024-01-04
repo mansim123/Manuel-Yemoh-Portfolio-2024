9a65d3da42753cacf0a9118258bb4f6792d299ad
Manuel yemoh <manuelyemoh@gmail.com>
2024-01-04 16:37:33 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Hero from "./Sections/Hero";
+import Toggle from "react-toggle";
+import { useState } from "react";
+import "react-toggle/style.css"; 
 
 function App() {
+  const [isDark, setIsDark] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`bg-${isDark ? "[#000000]" : "[#ffffff]"} w-screen h-screen transition-all duration-200`}>
+      <div className="fixed top-4 right-4 z-10"> 
+        <Toggle
+          checked={isDark}
+          onChange={({ target }) => setIsDark(target.checked)}
+          icons={{ checked: "🌙", unchecked: "🔆" }}
+          aria-label="Dark mode toggle"
+        />
+      </div>
+      <div className="fixed top-1/2 transform -translate-y-1/2 left-0 hidden opacity-0">
+          <div className={`w-[50px] h-[7px] ${isDark ? 'bg-[#ffffff]' : 'bg-[#000000]'}`}></div>
+          <div className={`w-[25px] h-[7px] mt-3 ${isDark ? 'bg-[#ffffff] opacity-60' : 'bg-[#000000] opacity-60'} transition-all duration-200`}></div>
+          <div className={`w-[25px] h-[7px] mt-3 ${isDark ? 'bg-[#ffffff] opacity-60' : 'bg-[#000000] opacity-60'} transition-all duration-200`}></div>
+          <div className={`w-[25px] h-[7px] mt-3 ${isDark ? 'bg-[#ffffff] opacity-60' : 'bg-[#000000] opacity-60'} transition-all duration-200`}></div>
+      </div>
+
+      {/* Pass isDark as a prop to Hero */}
+      <Hero isDark={isDark} />
     </div>
   );
 }
---
+++ src/Sections/Hero.js
@@ -0,0 +1,122 @@
+import React from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import { useEffect, useState } from "react";
+
+function Hero(props) {
+
+  const [isToggled, setIsToggled] = useState("opacity-0 duration-0");
+
+  useEffect(() => {
+    // You can place the code you want to run here
+    if (props.isDark) {
+
+        setIsToggled("opacity-0 duration-0");
+    } else {
+        setIsToggled("opacity-0 duration-0");
+
+    }
+  }, [props.isDark]); // Use props.isDark as a dependency
+
+  const particlesInit = async (main) => {
+    console.log(main);
+    await loadFull(main);
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+    setTimeout(() => {
+      setIsToggled("opacity-100 duration-200");
+    }, 50);
+  };
+
+  return (
+    <>
+      <div id="particles-js" className="w-screen h-screen">
+      <div className="absolute inset-0 flex justify-center items-center top-[-4rem]">
+        <div>
+          <h1 className={`font-Roboto font-black xs:leading-[1.2] xs:text-[4rem] md:text-[6rem] lg:text-[8rem] xxl:text-[11rem] text-center ${props.isDark ? 'text-[#ffffff]' : 'text-[#000000]'} transition-all duration-200`}>MANUEL YEMOH</h1>
+          <h2 className={`font-Roboto font-black xs:text-[2rem] md:text-[3.5rem] lg:text-[5rem] xxl:text-[7rem] text-center ${props.isDark ? 'text-[#ffffff]' : 'text-[#000000]'} transition-all duration-200`}>Front End Developer</h2>
+        </div>
+      </div>
+
+        <Particles
+          className={`${isToggled} transition-all`}
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            fpsLimit: 120,
+            pauseOnOutsideViewport: true,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 150,
+                  duration: 10,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "#ffffff",
+              },
+              links: {
+                color: "#ffffff",
+                distance: 250,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+              },
+              collisions: {
+                enable: true,
+              },
+              move: {
+                direction: "none",
+                enable: true,
+                outModes: {
+                  default: "bounce",
+                },
+                random: true,
+                speed: 2,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 1400,
+                },
+                value: 150,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "triangle",
+              },
+              size: {
+                value: { min: 1, max: 6 },
+              },
+            },
+            detectRetina: true,
+          }}
+        />
+      </div>
+    </>
+  );
+}
+
+export default Hero;
